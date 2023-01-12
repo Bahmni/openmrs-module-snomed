bb9a02c21048afd3929a41f7d9cafdb3c87037b4
manimaarans <manimaaran@beehyv.com>
2023-01-12 19:10:16 +0530

    BS-44 | added missed invoking super for custom exception

--- api/src/main/java/org/bahmni/module/terminologyservices/utils/TerminologyServerUnavailableException.java
+++ api/src/main/java/org/bahmni/module/terminologyservices/utils/TerminologyServerUnavailableException.java
@@ -2,5 +2,6 @@ package org.bahmni.module.terminologyservices.utils;
 
 public class TerminologyServerUnavailableException extends Exception{
     public TerminologyServerUnavailableException(String terminologyServerDownErrorMessage) {
+        super(terminologyServerDownErrorMessage);
     }
 }
